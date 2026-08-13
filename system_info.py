f5dc3cba836a5dca537dccd908dd4ace6067d748
Augustin <votre.email@example.com>
2026-08-14 00:41:34 +0100

    Initial comit:system information tool

---
+++ system_info.py
@@ -0,0 +1,36 @@
+import platform 
+import sys
+
+
+def get_computer_name():
+    return f"Computer:{ platform.node()}"
+
+def get_os_version():
+    return f"Machine:{platform.machine()}"
+
+def get_operating_system():
+    return f"OS:{platform.system()}"
+
+def get_python_version():
+    return f"Python:{sys.version.split()[0]}"
+
+
+def show_system_info():
+   print("SYSTEM INFORMATION")
+   print("=" *40)
+   print(get_computer_name())
+   print(get_os_version())
+   print(get_operating_system())
+   print(get_python_version())
+   print("=" *40)
+
+
+show_system_info()
+
+
+
+
+
+
+
+
