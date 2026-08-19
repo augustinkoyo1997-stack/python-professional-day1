bba9cfeddb882c92654bad50f4b8cd3327f2a529
Augustin <augustinkoyo1997@gmail.com>
2026-08-19 17:43:14 +0100

    Mise à jour du  fichier

--- system_info.py
+++ system_info.py
@@ -31,6 +31,6 @@ def main():
 print("Git tranning--version 2")
 print("version  developpée par le develeloppeur A et développée B") 
 print("System Information Tool - Professional Edition")
-print("Deleted de lin chemin = r'C:\Users\Augustin\python-professional-day1' in the system_info")
+print("Deleted de lin chemin  in the system_info")
 print("Modification faite directement sur GitHub")
 print("Modification  du  developpeur principal")
